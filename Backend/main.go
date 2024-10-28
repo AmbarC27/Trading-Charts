26310148bafe66b69862566db57b0886369ec380
AmbarC27 <ambarchakra@gmail.com>
2024-10-28 01:00:07 -0400

    Initial GET method working

--- Backend/main.go
+++ Backend/main.go
@@ -4,13 +4,30 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-func main() {
+type Stock struct {
+	Datetime    time.Time `json:"datetime"`
+	Open        float64   `json:"open"`
+	High        float64   `json:"high"`
+	Low         float64   `json:"low"`
+	Close       float64   `json:"close"`
+	Volume      int64     `json:"volume"`
+	Dividends   float64   `json:"dividends"`
+	StockSplits float64   `json:"stock_splits"`
+	Ticker      string    `json:"ticker"`
+}
+
+var db *sql.DB
+
+func initDB() {
 	// Load the environment variables
 	err := godotenv.Load("../Data_Store/.env")
 	if err != nil {
@@ -27,17 +44,59 @@ func main() {
 		user, password, dbname, host, port)
 
 	// Open a connection to the database
-	db, err := sql.Open("postgres", connStr)
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getAllStocks(c *gin.Context) {
+	rows, err := db.Query("SELECT * FROM stocks")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error retrieving data"})
+		return
+	}
+	defer rows.Close()
+
+	var stocks []Stock
+
+	for rows.Next() {
+		var stock Stock
+		err := rows.Scan(
+			&stock.Datetime,
+			&stock.Open,
+			&stock.High,
+			&stock.Low,
+			&stock.Close,
+			&stock.Volume,
+			&stock.Dividends,
+			&stock.StockSplits,
+			&stock.Ticker,
+		)
+
+		// Check if there was an error during the scan
+		if err != nil {
+			// Return an error response if scanning failed
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error scanning data"})
+			return
+		}
+		stocks = append(stocks, stock)
+	}
+
+	if err = rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error during row iteration"})
+		return
+	}
+
+	// Respond with the retrieved stocks as JSON
+	c.JSON(http.StatusOK, stocks)
+}
+
+func main() {
+	initDB()
 	defer db.Close()
 
-	// Verify the connection
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Successfully connected to PostgreSQL!")
+	r := gin.Default()
+	r.GET("/stocks", getAllStocks)
+	r.Run(":8080")
 }
