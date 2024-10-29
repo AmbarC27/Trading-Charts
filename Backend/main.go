e142e4f93e87a4a072fceb196da829f0f56797e0
AmbarC27 <ambarchakra@gmail.com>
2024-10-29 00:51:17 -0400

    Added GET methods for getting latest ticker data and getting all data for a particular ticker

--- Backend/main.go
+++ Backend/main.go
@@ -92,11 +92,113 @@ func getAllStocks(c *gin.Context) {
 	c.JSON(http.StatusOK, stocks)
 }
 
+func getLatestTickerData(c *gin.Context) {
+	query := `
+        SELECT DISTINCT ON (ticker) *
+        FROM stocks
+        ORDER BY ticker, Datetime DESC;
+    `
+
+	rows, err := db.Query(query)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error retrieving data"})
+		return
+	}
+	defer rows.Close()
+
+	var stockData []Stock
+
+	for rows.Next() {
+		var stock Stock
+		err := rows.Scan(
+			&stock.Datetime,
+			&stock.Open,
+			&stock.High,
+			&stock.Low,
+			&stock.Close,
+			&stock.Volume,
+			&stock.Dividends,
+			&stock.StockSplits,
+			&stock.Ticker,
+		)
+
+		// Check if there was an error during the scan
+		if err != nil {
+			// Return an error response if scanning failed
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error scanning data"})
+			return
+		}
+		stockData = append(stockData, stock)
+	}
+
+	if err = rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error during row iteration"})
+		return
+	}
+
+	// Return the result as a JSON array
+	c.JSON(http.StatusOK, stockData)
+}
+
+func getStockDataByTicker(c *gin.Context) {
+	// Get the ticker from the URL parameter
+	ticker := c.Param("ticker")
+
+	query := `
+        SELECT *
+        FROM stocks
+        WHERE ticker = $1;
+    `
+
+	rows, err := db.Query(query, ticker)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error retrieving data"})
+		return
+	}
+	defer rows.Close()
+
+	var stockData []Stock
+
+	// Iterate over the rows and scan them into the Stock struct
+	for rows.Next() {
+		var stock Stock
+		err := rows.Scan(
+			&stock.Datetime,
+			&stock.Open,
+			&stock.High,
+			&stock.Low,
+			&stock.Close,
+			&stock.Volume,
+			&stock.Dividends,
+			&stock.StockSplits,
+			&stock.Ticker,
+		)
+
+		// Check if there was an error during the scan
+		if err != nil {
+			// Return an error response if scanning failed
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error scanning data"})
+			return
+		}
+		stockData = append(stockData, stock)
+	}
+
+	if err = rows.Err(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error during row iteration"})
+		return
+	}
+
+	// Return the result as a JSON array
+	c.JSON(http.StatusOK, stockData)
+}
+
 func main() {
 	initDB()
 	defer db.Close()
 
 	r := gin.Default()
 	r.GET("/stocks", getAllStocks)
+	r.GET("/latest", getLatestTickerData)
+	r.GET("/stocks/:ticker", getStockDataByTicker)
 	r.Run(":8080")
 }
